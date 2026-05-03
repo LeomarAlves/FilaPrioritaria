59c1d2213cc0a66e2e08f4cf2c4f681c63fd93eb
Leomar Alves Marcelino <leo.dev.jobs@gmail.com>
2026-05-03 03:03:23 -0300

    feat: cria funções de enfileirar e desenfileirar

--- src/fila.cpp
+++ src/fila.cpp
@@ -2,11 +2,41 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 FilaCircurlar* criaFilaCircurlar() {
     FilaCircurlar* fila = new FilaCircurlar;
+    Pessoa array [capacidade];
     fila->inicio = 0;
     fila->final = 0;
     fila->quantidade = 0;
     
     return fila; 
+}
+
+void enfileirar(FilaCircurlar &fila, Pessoa p){
+
+    if (fila.quantidade == capacidade){
+        cout << "Fila está cheia! " << endl;
+        return;
+    }
+
+    fila.array[fila.final] = p;
+    fila.final = (fila.final + 1) % capacidade;
+    fila.quantidade++;
+}
+
+Pessoa desenfileirar(FilaCircurlar &fila){
+
+    if (fila.quantidade == 0){
+        cout << "Fila está vazia! " << endl;
+        Pessoa pessoaVazia = {"", -1, false};
+        return pessoaVazia;
+    }
+
+      Pessoa pessoaRemovida = fila.array[fila.inicio];
+      fila.inicio = (fila.inicio + 1) % capacidade;
+      fila.quantidade--;
+
+      return pessoaRemovida;
 }
